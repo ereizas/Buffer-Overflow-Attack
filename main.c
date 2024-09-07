fb848ca1dd3f372585ef776398c1a253b5040a70
ereizas <ereizas@gmail.com>
2024-09-07 17:34:31 -0400

    Added prompt for name

--- main.c
+++ main.c
@@ -5,5 +5,10 @@ int main(int argc, char *argv[]){
     char B[10];
     char s1[] = "I owe you $1000";
     char s2[] = "I don't know you";
+    while(1){
+        printf("What's your name? ");
+        gets(B);
+        printf("Name is %s\n", B);
+    }
     return 0;
 }
