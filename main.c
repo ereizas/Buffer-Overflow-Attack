75d5a3d62a574a40ed7eeddc5ddc4c2ebd01f73f
ereizas <ereizas@gmail.com>
2024-09-07 17:43:40 -0400

    Added conditional prints

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *argv[]){
     char B[10];
@@ -8,7 +9,12 @@ int main(int argc, char *argv[]){
     while(1){
         printf("What's your name? ");
         gets(B);
-        printf("Name is %s\n", B);
+        if(strstr(B,"joker")){
+            printf("%s\n",s1);
+        }
+        else{
+            printf("%s\n",s2);
+        }
     }
     return 0;
 }
